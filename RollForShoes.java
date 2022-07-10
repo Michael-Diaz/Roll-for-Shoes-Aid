a26e36281f3452a247c1191d531cf35f1e1a9b73
Michael Diaz <michaeldiaz.compsci@gmail.com>
2022-07-10 16:00:07 -0400

    Initial File Setup
    Creation of base file for the entire aid

---
+++ RollForShoes.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class RollForShoes
+{
+    static ArrayList<Character> cast;
+
+    public static void main(String[] args)
+    {
+        cast = new ArrayList<Character>();
+
+        // output some flavortext on startup
+        // run the prompt() function
+    }
+
+
+    // ========== STARTUP ==========
+    public void promptFile()
+    {
+        // searches for exisitng 'save files'
+        // no file found runs the buildCast() method, else...
+        // pulls up 'saves' and prompts user to select a save
+        // selecting a save loads the players[] array with the respective characters else...
+        // run buildCast method
+    }
+
+    public void buildCast()
+    {
+        // ask for number of characters, for each character...
+        // ask for name & pronouns
+        // initialize health to 6, xp to 0, empty inventory, single skill: Do Something (1) then...
+        // display cast list, allow editing before confirming
+        // add characters to players[] array
+    }
+
+
+    // ========== GAMEPLAY ==========
+    
+}
