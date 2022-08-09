fd1fda7cf29b82480de7797beafa8b526d243cab
Michael Diaz <michaeldiaz.compsci@gmail.com>
2022-08-08 22:07:23 -0400

    File Read Capabilities
    Imported json.simple library
    
    Began writing "loadCast()" function
    -Still need to create player objects w/ constructors
    
    Completed the "parseSaveFile()" function
    -Loads in JSON save file
    -Extracts data from relevant fields in placeholder variables
    
    Altered author_save.json and testing_save.json
    -Rewrote format for testing_save.json to use json.simple library
    -Rewrote author_save.json to fit above format

--- RollForShoes.java
+++ RollForShoes.java
@@ -1,3 +1,10 @@
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
@@ -7,6 +14,10 @@ public class RollForShoes
     static Scanner userInput;
     static ArrayList<Character> cast;
 
+    /* Runs the main program, displaying the initial flavortext,
+     * assigning the scanner and cast array, and prompts a file
+     * to be selected, if any exist.
+     */
     public static void main(String[] args)
     {
         userInput = new Scanner(System.in);
@@ -26,10 +37,15 @@ public class RollForShoes
 
 
     // ========== STARTUP ==========
-    static public void promptFile()
+    /* Searches the save directory for existing JSONs to be loaded,
+     * otherwise directs the user to create a new save (option is
+     * also offered even if saves are detected).
+     */
+    public static void promptFile()
     {
         String[] pathnames;
-        File f = new File("C:/Users/HP/Documents/Github/Roll-for-Shoes-Aid/Save Files");
+        String root = "C:/Users/HP/Documents/Github/Roll-for-Shoes-Aid/Save Files/";
+        File f = new File(root);
 
         FilenameFilter filter = new FilenameFilter() 
         {
@@ -42,18 +58,22 @@ public class RollForShoes
 
         pathnames = f.list(filter);
 
-        if (pathnames.length == 0)
+        if (pathnames == null)
+        {
+            System.out.println("Loading Saves... \n"
+                                + "No saves found, creating a new save slot...");
             buildCast();
+        }
         else
         {
             int fileChoice;
 
-            System.out.println("Loading Saves...");
+            System.out.print("Loading Saves...");
             while (true)
             {
                 try
                 {
-                    System.out.println("Save(s) found, select a file [#] or '0' to create a new save: \n"
+                    System.out.println("\nSave(s) found, select a file [#] or '0' to create a new save: \n"
                                         + "-------------------------------------------------------------");
 
                     int fileNum = 1;
@@ -68,42 +88,159 @@ public class RollForShoes
                     fileChoice = userInput.nextInt();
 
                     if (fileChoice < 0 || fileChoice > pathnames.length)
-                        System.out.println("\n!!! {INPUT INVALID} !!!\n");
-                    else if (fileChoice == 0)
+                        System.out.println("\n!!! {INPUT INVALID} !!!");
+                    else if (confirmInput(String.valueOf(fileChoice)))
                     {
-                        buildCast();
-                        break;
-                    }
-                    else
-                    {
-                        loadCast();
-                        break;
+                        if (fileChoice == 0)
+                        {
+                            System.out.println("\nCreating a new save slot...");
+                            buildCast();
+                            break;
+                        }
+                        else
+                        {
+                            System.out.println("\nLoading save slot...");
+                            loadCast(root + pathnames[fileChoice - 1]);
+                            break;
+                        }
                     }
                 }
                 catch (Exception e)
                 {
-                    System.out.println("\n!!! {INPUT INVALID} !!!\n");
+                    System.out.println("\n!!! {INPUT INVALID} !!!");
                     userInput.next();
+                    e.printStackTrace();
                 }
             }
         }
-
-        // selecting a save loads the players[] array with the respective characters else...
-        // run buildCast method
     }
 
-    static public void loadCast()
+    public static void loadCast(String path)
     {
+        System.out.println(path);
+        JSONParser jsonParser = new JSONParser();
+         
+        try (FileReader reader = new FileReader(path))
+        {
+            //Read JSON file
+            Object obj = jsonParser.parse(reader);
+            JSONArray castList = (JSONArray) obj;
 
+            for (int i = 0; i < castList.size(); i++)
+            {
+                JSONObject playerChar = (JSONObject)castList.get(i);
+                parseSaveFile(playerChar);
+            }
+        }
+        catch (IOException e) 
+        {
+            System.out.println("\n!!! {ERROR READING FILE} !!!");
+            e.printStackTrace();
+        } 
+        catch (ParseException e) 
+        {
+            System.out.println("\n!!! {ERROR READING FILE} !!!");
+            e.printStackTrace();
+        }
+
+        // load the JSON file the user chose
+        // extract the # of PCs in the file, for each one...
+        // either extract the data into variables and run a constructor...
+        // or find a way to turn the structure into an object directly
+        // add each 
     }
 
-    static public void buildCast()
+    public static void parseSaveFile(JSONObject character)
+    {
+        //Get player character object within list
+        JSONObject charObject = (JSONObject) character.get("character");
+         
+        String name = (String) charObject.get("name");    
+        System.out.println(name);
+
+        ArrayList<String> pronouns = new ArrayList<String>();
+        JSONArray jPronouns = (JSONArray) charObject.get("pronouns");
+        for (Object jPnoun : jPronouns) 
+        {
+            System.out.println(jPnoun.toString());
+            pronouns.add(jPnoun.toString());
+        }
+
+        String skill;
+        long skillLvl;
+        JSONArray skills = (JSONArray) charObject.get("skills");
+        for (Object ability : skills)
+        {
+            JSONObject jAbility = (JSONObject) ability;
+            skill = (String) jAbility.get("skill");
+            skillLvl = (long) jAbility.get("level");
+            System.out.println(skill + ", lvl: " + skillLvl);
+        }
+        
+        long xp = (long) charObject.get("xp");    
+        System.out.println(xp);
+
+        String itemName;
+        String itemDesc;
+        JSONArray inventory = (JSONArray) charObject.get("inventory");
+        for (Object item : inventory)
+        {
+            JSONObject jItem = (JSONObject) item;
+            itemName = (String) jItem.get("item");
+            itemDesc = (String) jItem.get("description");
+            System.out.println(itemName + ": \t" + itemDesc);
+        }
+    }
+
+    public static void buildCast()
     {
         // ask for number of characters, for each character...
         // ask for name & pronouns
         // initialize xp to 0, empty inventory, single skill: Do Something (1) then...
         // display cast list, allow editing before confirming
         // add characters to players[] array
+    }
+
+    
+    // ========== INPUT ==========
+    /* After selecting a option from multiple presented, the user
+     * will be asked to verify their choice; a return of true breaks
+     * and submits choice, false will re-prompt with the question.
+     */
+    public static boolean confirmInput(String input)
+    {
+        String confirm;
+        String border = "--------------------------------";
+        for (int i = 0; i < input.length() + 1; i++)
+        {
+            border += "-";
+        }
+
+        while (true)
+        {
+            try
+            {
+                System.out.println("\nConfirm your selection [Y/N]: '" 
+                                    + input + "'? \n"
+                                    + border);
+
+                System.out.print(">_: ");
+                confirm = userInput.next();
+                String confirmL = confirm.toLowerCase();
+
+                if (!(confirmL.equals("y") || confirmL.equals("n")))
+                    System.out.println("\n!!! {INPUT INVALID} !!!");
+                else if (confirmL.equals("y"))
+                    return true;
+                else
+                    return false;
+            }
+            catch (Exception e)
+            {
+                System.out.println("\n!!! {INPUT INVALID} !!!");
+                userInput.next();
+            }
+        }
     }
 
 
